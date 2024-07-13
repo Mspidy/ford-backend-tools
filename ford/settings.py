b2a7fdcbbc1a40d1f64f1418255e4c7b5674a09b
Prabhat <prabhat.cioreview@gmail.com>
2024-07-13 02:16:47 -0700

    modifications with first new models of Teacher Registrations

--- ford/settings.py
+++ ford/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myapp'
+    'myapp',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -74,10 +75,21 @@ WSGI_APPLICATION = 'ford.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'forddb', #name
+        'USER': 'postgres',     #user
+        'PASSWORD': 'root123', #password
+        'HOST': 'localhost',  #host, typically 'localhost' or '127.0.0.1'
+        'PORT': '5432',       #port, typically '5432'
     }
 }
 
--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class TeacherDetails(models.Model):
+    teacher_id = models.CharField(max_length=100)
+    teacher_name = models.CharField(max_length=100)
+    father_name = models.CharField(max_length=100)
+    mother_name = models.CharField(max_length=100)
+    pan_card = models.CharField(max_length=100)
+    aadhar_card = models.CharField(max_length=100)
+    mobile_number = models.IntegerField(blank=True, default=0)
+    alter_number = models.IntegerField(blank=True, default=0)
+    date_of_birth = models.CharField(max_length=100)
+    previous_school_name = models.CharField(max_length=250)
+    transport_fees = models.IntegerField(blank=True, default=0)
+    graduation_complete = models.BooleanField(default=False)
+    bed_complete = models.BooleanField(default=False)
+    created_by = models.CharField(max_length=100)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if not self.teacher_id:
+            last_instance = TeacherDetails.objects.order_by('-teacher_id').first()
+            if last_instance:
+                last_sequence = int(last_instance.teacher_id[2:])
+                next_sequence = last_sequence + 1
+                self.teacher_id = f"TR{str(next_sequence).zfill(4)}"
+            else:
+                self.teacher_id = "TR0001"
+        super().save(*args, **kwargs)
