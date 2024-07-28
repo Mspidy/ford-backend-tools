d761ce641edcc3f555188e13d7c53442a4861b82
Prabhat <prabhat.cioreview@gmail.com>
2024-07-28 00:30:57 -0700

    modifications with new changes of ford backend with teacher registerations API

--- ford/urls.py
+++ ford/urls.py
@@ -16,9 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from myapp.views import TeacherDetailsListCreateAPIView
+from django.conf.urls.static import static
+from django.conf import settings
+from myapp.views import TeacherDetailsListCreateAPIView, BulkTeacherUploadView, DownloadTeacherTemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('teacher-details/', TeacherDetailsListCreateAPIView.as_view(), name='teacher-details-list-create'),
+    path('upload-teachers/', BulkTeacherUploadView.as_view(), name='upload-teachers'),
+    path('download-teacher-template/', DownloadTeacherTemplateView.as_view(), name='download-teacher-template'),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- myapp/views.py
+++ myapp/views.py
@@ -5,6 +5,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import TeacherDetails
+from django.http import FileResponse
+import pandas as pd
+import os
+from django.conf import settings
+from datetime import datetime
 
 # Create your views here.
 class TeacherDetailsListCreateAPIView(APIView):
@@ -19,3 +24,46 @@ class TeacherDetailsListCreateAPIView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class BulkTeacherUploadView(APIView):
+    def post(self, request, *args, **kwargs):
+        file = request.FILES['file']
+        try:
+            data = pd.read_excel(file)
+            for _, row in data.iterrows():
+                dob_str = row['date_of_birth'].strftime('%d-%m-%Y') if isinstance(row['date_of_birth'], pd.Timestamp) else row['date_of_birth']
+                dob_date = datetime.strptime(dob_str, '%d-%m-%Y').date()
+                teacher_data = {
+                    'teacher_name': row['teacher_name'],
+                    'father_name': row['father_name'],
+                    'mother_name': row['mother_name'],
+                    'pan_card': row['pan_card'],
+                    'aadhar_card': row['aadhar_card'],
+                    'mobile_number': row['mobile_number'],
+                    'subject': row['subject'],
+                    'salary': row['salary'],
+                    'alter_number': row['alter_number'],
+                    'date_of_birth': dob_date,
+                    'previous_school_name': row['previous_school_name'],
+                    'transport_fees': row['transport_fees'],
+                    'created_by': 'Anand'
+                }
+                serializer = TeacherSerializer(data=teacher_data)
+                if serializer.is_valid():
+                    serializer.save()
+                else:
+                    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"message": "Teachers uploaded successfully"}, status=status.HTTP_201_CREATED)
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+class DownloadTeacherTemplateView(APIView):
+    def get(self, request, *args, **kwargs):
+        file_path = os.path.join(settings.MEDIA_ROOT, 'teacher', 'teacher_registeration.xlsx')
+        
+        if os.path.exists(file_path):
+            response = FileResponse(open(file_path, 'rb'), content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+            response['Content-Disposition'] = 'attachment; filename="teacher_registeration.xlsx"'
+            return response
+        else:
+            return Response({"error": "File not found"}, status=status.HTTP_404_NOT_FOUND)
